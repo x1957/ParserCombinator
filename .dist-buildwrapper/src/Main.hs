83276ede0d0017fa8b26f0766405a8963644b6a1
x1957 <fangyuhao@gmail.com>
2013-12-16 11:12:14 +0800

    not using monad

--- .dist-buildwrapper/src/Main.hs
+++ .dist-buildwrapper/src/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
 
-type Paser a = String -> [(a , String)]
+type Parser a = String -> [(a , String)]
+
+--data Parser a = Parser (String -> [(a , String)])
 
 result :: a -> Parser a
 result v = \inp -> [(v , inp)]
@@ -16,6 +18,20 @@ item = \inp  case inp of
         
         
 seq :: Parser a -> Parser b -> Parser (a , b)
+a `seq` b = \inp -> [((v1,v2) , inp'') | (v1 , inp') <- a inp , (v2,inp'') <- b inp']
+
+bind :: Parser a -> (a -> Parser b) -> Parser b
+p `bind` f = \inp -> concat [f v inp' | (v ,inp') <- p inp]
+
+plus :: Parser a -> Parser a -> Parser a
+p `plus` q = \inp -> (p inp ++ q inp)
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = item `bind` \x ->
+        if p x then result x else zero
+
+char :: Char -> Parser Char
+char x = sat (\y -> x == y)
 
 main::IO()
 main = undefined
